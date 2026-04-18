cff5cc1a03b9f98c5a44eefedd0ee7050d4ef9fb
logisticamais19-lang <logisticamais19@gmail.com>
2026-04-18 22:54:13 +0000

    fix: corrige import do authenticate no adminRoutes

--- backend/functions/routes/adminRoutes.js
+++ backend/functions/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const authenticate = require('../middleware/auth')
+const { authenticate } = require('../middleware/auth')
 const adminCtrl = require('../controllers/adminController')
 
 const ADMIN_UIDS = ['2WKwdCPEJoMocxRv8cOdZn31kjn2']
